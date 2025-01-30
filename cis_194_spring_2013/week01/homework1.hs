2aecfeb801c33328cbab909e881efb720b780713
Adamocho <81510507+Adamocho@users.noreply.github.com>
2025-01-30 12:55:39 +0100

    Write week01 anew

--- cis_194_spring_2013/week01/homework1.hs
+++ cis_194_spring_2013/week01/homework1.hs
@@ -1,43 +1,33 @@
--- Homework 1
+{-# OPTIONS_GHC -Wall #-}
 
--- Bank Card Number Validator
--- Exercise 1
-toDigitsRev    :: Integer -> [Integer]
-toDigitsRev -x = []
-toDigitsRev 0 = []
-toDigitsRev x = (x `mod` 10) : toDigitsRev (x `div` 10)
+-- this checks if the supplied bank account number is correct or not --
+validate :: Integer -> Bool
+validate x = sumDigits (doubleEveryOther (toDigitsRev x)) `mod` 10 == 0
 
-toDigits    :: Integer -> [Integer]
-toDigits xs = reverse (toDigitsRev xs)
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x
+    | x <= 0 = []
+    | otherwise = mod x 10 : toDigitsRev (x `div` 10)
 
--- Exercise 2
+toDigits :: Integer -> [Integer]
+toDigits x = reverse (toDigitsRev x)
+
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = reverse (doubleEveryOtherRev (reverse xs))
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x:y:xs) = x : 2 * y : doubleEveryOther xs
 
-doubleEveryOtherRev :: [Integer] -> [Integer]
-doubleEveryOtherRev [] = []
-doubleEveryOtherRev [x] = [x]
-doubleEveryOtherRev (x:y:[]) = [x, y * 2]
-doubleEveryOtherRev (x:y:xs) = x : (y * 2) : (doubleEveryOtherRev xs)
-
--- Exercise 3
+-- sum EACH digit of the number present in the list --
 sumDigits :: [Integer] -> Integer
 sumDigits [] = 0
-sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
+sumDigits [x]
+    | x > 9 = x `mod` 10 + x `div` 10
+    | otherwise = x
+sumDigits (x:xs) = sumDigits [x] + sumDigits xs
 
--- Exercise 4
-validate :: Integer -> Bool 
-validate x = sumDigits (doubleEveryOther (toDigits x)) `mod` 10 == 0
-
--- Hanoi Tower Move Calculator
--- Exercise 5
+-- hanoi tower --
 type Peg = String
-type Move = (Peg, Peg) 
+type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi 0 _ _ _ = []
-hanoi 1 a b c = [(a, c)]
-hanoi 2 a b c = [(a, b), (a, c), (b, c)]
-hanoi x a b c = hanoi (subtract 1 x) a c b ++ hanoi 1 a b c ++ hanoi (subtract 1 x) b a c
-
--- Exercise 6??? 
-introduce hanoi with 4 pegs
+hanoi 1 start _ end = [(start, end)]
+hanoi n start middle end = hanoi (n - 1) start end middle ++ [(start, end)] ++ hanoi (n - 1) middle start end
